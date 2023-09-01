a7c538ab9fd73ca7d7e4517d946709d62d144983
Gabriel Vallejo <gabi.vallejo44@gmail.com>
2023-09-01 22:26:14 +0200

    Herramienta escanerPuertos actualizada

--- escanerPuertos.sh
+++ escanerPuertos.sh
@@ -1,16 +1,33 @@
 #!/bin/bash
 
-function ctrl_c() {
-	echo -e "\n[!] Saliendo...\n"
-	tput cnorm; exit 1
+function ctrl_c(){
+    echo -e "\n\n[!] Saliendo...\n"
+    exit 1
 }
 
-# Ctrl + C
-trap ctrl_c INT
+# Ctrl+C
+trap ctrl_c SIGINT
 
-tput civis # Oculta el cursor
-for puerto in $(seq 1 65535); do
-	(echo '' > /dev/tcp/127.0.0.1/$puerto) 2>/dev/null && echo "[+] $puerto - OPEN" || echo "[+] $puerto - CLOSE" &
-done; wait
+declare -a ports=( $(seq 1 65535) )
 
-tput cnorm # Recuperamos el cursor
+function checkPort() {
+    #abro un descriptor de archivos con permisos de escritura y lectura
+    (exec 3<> /dev/tcp/$1/$2) 2>/dev/null
+
+    if [ $? -eq 0 ]; then
+        echo -e "[+] Host $1 - port $2 (OPEN)"
+    fi
+    #cierro el descriptor, lo cierro de las dos formas para asegurarme de que se cierra
+    exec 3<&-
+    exec 3>&-
+}
+
+if [ $1 ]; then
+    for port in ${ports[@]}; do
+        checkPort $1 $port &
+    done
+else
+    echo -e "\n[!] Uso: $0 <ip-address>\n" 
+fi
+
+wait
